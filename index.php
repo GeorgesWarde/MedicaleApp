afba9203c9f58408e74ec60df311f8a99f1fd3aa
GeorgesWarde <warde-georges123@outlook.com>
2022-05-18 13:04:37 +0300

    Some changes

---
+++ index.php
@@ -0,0 +1,325 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>Navbar Template · Bootstrap v5.1</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+   <!-- ===== Link Swiper's CSS ===== -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/>
+
+   <!-- ===== Fontawesome CDN Link ===== -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"/>
+       
+    
+
+    
+    <lin href="cards.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+  </head>
+  <body><header>
+    <nav class="navbar navbar-expand-lg navbar-light " aria-label="Fifth navbar example">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">MedicalCenter</a>
+          <button class="navbar-toggler menu-button" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+    
+          <div class="collapse navbar-collapse  " id="navbarsExample05">
+           
+            <ul class="navbar-nav f">
+              
+              <li class="nav-item dropdown navbar-items ">
+
+                <a class="nav-link dropdown-toggle " href="#dropdown1" id="dropdown05" data-bs-toggle="collapse" data-bs-target="#dropdown1" aria-controls="dropdown" aria-expanded="false" aria-label="Toggle navigation">Dropdown</a>
+                <ul class="dropdown-menu collapse" id="dropdown1">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown navbar-items">
+
+              <a class="nav-link dropdown-toggle " href="#dropdown2" id="dropdown05" data-bs-toggle="collapse" data-bs-target="#dropdown2" aria-controls="dropdown" aria-expanded="false" aria-label="Toggle navigation">Dropdown</a>
+              <ul class="dropdown-menu collapse" id="dropdown2">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+          </li>
+          <li class="nav-item dropdown navbar-items">
+
+            <a class="nav-link dropdown-toggle " href="#dropdown3" id="dropdown05" data-bs-toggle="collapse" data-bs-target="#dropdown3" aria-controls="dropdown" aria-expanded="false" aria-label="Toggle navigation">Dropdown</a>
+            <ul class="dropdown-menu collapse" id="dropdown3">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+        </li>
+            </ul>
+    
+            <form class="searchBox">
+              <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+            </form>
+          </div>
+        </div>
+      </nav></header>
+      <div class="container firstWrapper">
+        <div class="row align-items-start">
+          <div class="col">
+            <h1>Putting Patients First</h1>
+            <div>Our cancer center team, led by Dr. Matthew Kulke, works every day in the fight against cancer. They go above and beyond because people need more and because it matters.
+
+              Learn more</div>
+          </div>
+          <div class="col">
+            <img src="IMAGES/patientpIC.jpg" width="450px" height="200px">
+          </div>
+          </div>
+        </div>
+        <div class="secondWrapper">
+
+          <a href="#" class="btn btn-primary" role="button" data-bs-toggle="button">Toggle link</a>
+<a href="#" class="btn btn-primary active" role="button" data-bs-toggle="button" aria-pressed="true">Active toggle link</a>
+
+        </div>
+        <div><h1 class="newsText">Latest News</h1>
+       <div class="thirdWrapper">
+        
+        <section>
+        <div class="product">
+            <picture>
+              <img src="images/patientpIC.jpg" alt="">
+            </picture>
+            <div class="detail">
+              <p class="text-size">
+              New Research from the Grayken Center for Addiction Suggests
+              
+</p>
+           
+          </div>
+           </div>
+           <div class="product">
+            <picture>
+              <img src="images/patientpIC.jpg" alt="">
+            </picture>
+            <div class="detail">
+              <p>
+                hey
+              </p>
+           
+          </div>
+           </div>
+           <div class="product">
+            <picture>
+              <img src="images/patientpIC.jpg" alt="">
+            </picture>
+            <div class="detail">
+              <p>
+                hey
+              </p>
+           
+          </div>
+           </div>
+           <div class="product">
+            <picture>
+              <img src="images/patientpIC.jpg" alt="">
+            </picture>
+            <div class="detail">
+              <p>
+                hey
+              </p>
+           
+          </div>
+           </div>
+          <div class="product">
+            <picture>
+              <img src="image/watch.png" alt="">
+            </picture>
+            <div class="detail">
+              <p>
+                <b>Product Five</b><br>
+                <small>New arrival</small>
+              </p>
+              <samp>$45.00</samp>
+            </div>
+            <div class="button">
+              <p class="star">
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+              </p>
+              <a href="#">Add-cart</a>
+            </div>
+          </div>
+          <div class="product">
+            <picture>
+              <img src="image/delta.png" alt="">
+            </picture>
+            <div class="detail">
+              <p>
+                <b>Product Six</b><br>
+                <small>New arrival</small>
+              </p>
+              <samp>$45.00</samp>
+            </div>
+            <div class="button">
+              <p class="star">
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+              </p>
+              <a href="#">Add-cart</a>
+            </div>
+          </div>
+          <div class="product">
+            <picture>
+              <img src="image/image3.png" alt="">
+            </picture>
+            <div class="detail">
+              <p>
+                <b>Product Seven</b><br>
+                <small>New arrival</small>
+              </p>
+              <samp>$45.00</samp>
+            </div>
+            <div class="button">
+              <p class="star">
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+              </p>
+              <a href="#">Add-cart</a>
+            </div>
+          </div>
+          <div class="product">
+            <picture>
+              <img src="image/image2.png" alt="">
+            </picture>
+            <div class="detail">
+              <p>
+                <b>Product Eight</b><br>
+                <small>New arrival</small>
+              </p>
+              <samp>$45.00</samp>
+            </div>
+            <div class="button">
+              <p class="star">
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+              </p>
+              <a href="#">Add-cart</a>
+            </div>
+          </div>
+          <div class="product">
+            <picture>
+              <img src="image/nokia.png" alt="">
+            </picture>
+            <div class="detail">
+              <p>
+                <b>Product Nine</b><br>
+                <small>New arrival</small>
+              </p>
+              <samp>$45.00</samp>
+            </div>
+            <div class="button">
+              <p class="star">
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+              </p>
+              <a href="#">Add-cart</a>
+            </div>
+          </div>
+          <div class="product">
+            <picture>
+              <img src="image/nokia-air.png" alt="">
+            </picture>
+            <div class="detail">
+              <p>
+                <b>Product Ten</b><br>
+                <small>New arrival</small>
+              </p>
+              <samp>$45.00</samp>
+            </div>
+            <div class="button">
+              <p class="star">
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+              </p>
+              <a href="#">Add-cart</a>
+            </div>
+          </div>
+          <div class="product">
+            <picture>
+              <img src="image/feature_3.png" alt="">
+            </picture>
+            <div class="detail">
+              <p>
+                <b>Product Eleven</b><br>
+                <small>New arrival</small>
+              </p>
+              <samp>$45.00</samp>
+            </div>
+            <div class="button">
+              <p class="star">
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+              </p>
+              <a href="#">Add-cart</a>
+            </div>
+          </div>
+          <div class="product">
+            <picture>
+              <img src="image/one.png" alt="">
+            </picture>
+            <div class="detail">
+              <p>
+                <b>Product Twelve</b><br>
+                <small>New arrival</small>
+              </p>
+              <samp>$45.00</samp>
+            </div>
+            <div class="button">
+              <p class="star">
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+                <strong>&star;</strong>
+              </p>
+              <a href="#">Add-cart</a>
+            </div>
+          </div>
+        </section>
+       </div></div>
+      <?php include_once 'footer.php';  ?>
+<script src="Js/main.js"></script>
+    
+  
+  
+
+</body>
+</html>  
