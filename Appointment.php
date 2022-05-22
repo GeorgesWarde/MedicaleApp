d9139fe5b9c1c169311b59b36d3bd9444c2f0fd4
GeorgesWarde <warde-georges123@outlook.com>
2022-05-22 20:27:48 +0300

    Appointment Added

---
+++ Appointment.php
@@ -0,0 +1,185 @@
+<html>
+    <head>
+<title>Appointments</title>
+<link rel="stylesheet" href="style.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    </head>
+    <body>
+    <?php include_once 'header.php';  ?>
+    <main class="sideBar">
+    <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white" style="width: 380px;">
+    <h5 style="padding-left:45px;" class="department">Find a Doctor by department</h5>
+    <ul class="nav nav-pills flex-column mb-auto departments">
+      <li class="nav-item department">
+        <a href="#" class="nav-link active" aria-current="page">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"/></svg>
+          Neurology
+        </a>
+      </li>
+      <li class="department">
+        <a href="#" class="nav-link active">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"/></svg>
+          Dentistry
+        </a>
+      </li>
+      <li class="department" >
+        <a href="#" class="nav-link active">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"/></svg>
+          Cardiovascular
+        </a>
+      </li>
+      <li class="department" >
+        <a href="#" class="nav-link active">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"/></svg>
+          Gastroenterology
+        </a>
+      </li>
+      <li class="department">
+        <a href="#" class="nav-link active">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"/></svg>
+          Dermatology
+        </a>
+      </li>
+    </ul>
+
+     
+
+    <a href="/" class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
+      <svg class="bi me-2" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
+      <span class="fs-5 fw-semibold">Appointments Schedules</span>
+    </a>
+    <div class="list-group list-group-flush border-bottom scrollarea">
+      <a href="#" class="list-group-item list-group-item-action active py-3 lh-tight" aria-current="true">
+        <div class="d-flex w-100 align-items-center justify-content-between relativePic">
+          <strong class="mb-1">Dr Georges warde</strong>
+          <small>Wed 8AM</small>
+          <img class="doctorIcon" src="Images/doctor.png" height="20px" width="20px">
+        </div>
+        <div class="col-10 mb-1 small">Neurology Specialist</div>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action py-3 lh-tight">
+        <div class="d-flex w-100 align-items-center justify-content-between relativePic">
+          <strong class="mb-1">Dr Anthony Saliba</strong>
+          <small class="text-muted">Tues 11AM</small>
+          <img class="doctorIcon" src="Images/doctor.png" height="20px" width="20px">
+        </div>
+        <div class="col-10 mb-1 small">Neurology Specialist</div>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action py-3 lh-tight">
+        <div class="d-flex w-100 align-items-center justify-content-between relativePic">
+          <strong class="mb-1">Dr Jules Warde</strong>
+          <small class="text-muted">Mon 11AM</small>
+          <img class="doctorIcon" src="Images/doctor.png" height="20px" width="20px">
+        </div>
+        <div class="col-10 mb-1 small">Neurology Specialist</div>
+      </a>
+
+      <a href="#" class="list-group-item list-group-item-action py-3 lh-tight" aria-current="true">
+        <div class="d-flex w-100 align-items-center justify-content-between relativePic">
+          <strong class="mb-1">Dr Elie Ghafary</strong>
+          <small class="text-muted">Wed 8AM</small>
+          <img class="doctorIcon" src="Images/doctor.png" height="20px" width="20px">
+        </div>
+        <div class="col-10 mb-1 small">Neurology Specialist</div>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action py-3 lh-tight">
+        <div class="d-flex w-100 align-items-center justify-content-between relativePic">
+          <strong class="mb-1">List group item heading</strong>
+          <small class="text-muted">Tues 5PM</small>
+          <img class="doctorIcon" src="Images/doctor.png" height="20px" width="20px">
+        </div>
+        <div class="col-10 mb-1 small">Neurology Specialist</div>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action py-3 lh-tight">
+        <div class="d-flex w-100 align-items-center justify-content-between relativePic">
+          <strong class="mb-1">Dr Charbel Warde</strong>
+          <small class="text-muted">Mon 3PM</small>
+          <img class="doctorIcon" src="Images/doctor.png" height="20px" width="20px">
+        </div>
+        <div class="col-10 mb-1 small">Neurology Specialist</div>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action py-3 lh-tight" aria-current="true">
+        <div class="d-flex w-100 align-items-center justify-content-between relativePic">
+          <strong class="mb-1">Dr Ralph Saliba</strong>
+          <small class="text-muted">Wed 6PM</small>
+          <img class="doctorIcon" src="Images/doctor.png" height="20px" width="20px">
+        </div>
+        <div class="col-10 mb-1 small">Neurology Specialist</div>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action py-3 lh-tight">
+        <div class="d-flex w-100 align-items-center justify-content-between relativePic">
+          <strong class="mb-1">Dr Elie Warde</strong>
+          <small class="text-muted">Tues 2PM</small>
+          <img class="doctorIcon" src="Images/doctor.png" height="20px" width="20px">
+        </div>
+        <div class="col-10 mb-1 small">Neurology Specialist</div>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action py-3 lh-tight">
+        <div class="d-flex w-100 align-items-center justify-content-between relativePic">
+          <strong class="mb-1">Dr Ali Mawla</strong>
+
+          <small class="text-muted">Mon 8PM</small>
+          <img class="doctorIcon" src="Images/doctor.png" height="20px" width="20px">
+        </div>
+        <div class="col-10 mb-1 small">Neurology Specialist</div>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action py-3 lh-tight" aria-current="true">
+        <div class="d-flex w-100 align-items-center justify-content-between relativePic">
+          <strong class="mb-1">Dr Omar Zeaiter</strong>
+          <small class="text-muted">Wed 2PM</small>
+          <img class="doctorIcon" src="Images/doctor.png" height="20px" width="20px">
+        </div>
+        <div class="col-10 mb-1 small">Neurology Specialist</div>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action py-3 lh-tight">
+        <div class="d-flex w-100 align-items-center justify-content-between relativePic">
+          <strong class="mb-1">Dr John Smith</strong>
+          <small class="text-muted">Tues 1AM</small>
+          <img class="doctorIcon" src="Images/doctor.png" height="20px" width="20px">
+        </div>
+        <div class="col-10 mb-1 small">Neurology Specialist</div>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action py-3 lh-tight">
+        <div class="d-flex w-100 align-items-center justify-content-between relativePic">
+          <strong class="mb-1">Dr Fadi Hanna</strong>
+          <small class="text-muted">Mon 12AM</small>
+          <img class="doctorIcon" src="Images/doctor.png" height="20px" width="20px">
+        </div>
+        <div class="col-10 mb-1 small">Neurology Specialist</div>
+      </a>
+    </div>
+  </div>
+
+  <div class="b-example-divider"></div>
+  <form class="mergeCenter">
+    
+    <h1 class="h3 mb-3 fw-normal">Fill your Request Please!</h1>
+
+    <div style="margin-top:10px;">
+  <label>Patient Name:</label>  <input style="border-radius:5px;width:300px;height:50px;margin-left:5px;" type="text" placeholder="Full Name">
+    </div>
+   
+    <div class="form-floating inputs">Location:
+    <select style="width:300px;border-radius:5px;height:50px;margin-left:40px;" name="location" id="locationMeeting"><option>Main Office</option></select>
+    </div>
+    <div class="form-floating inputs">
+    Schedule Time:
+    <select style="width:300px;border-radius:5px;height:50px;" name="location" id="locationMeeting"><option>8Pm</option></select>
+  
+    </div><div style="margin-top:10px;">
+  <label>Duration:</label>  <input style="border-radius:5px;width:300px;height:50px;margin-left:40px;" type="text" placeholder="Duration">
+    </div>
+   
+    <button style="margin-top:50px;" class="w-100 btn btn-lg btn-primary" type="submit">Request</button>
+    
+  </form>
+</main>
+
+
+
+
+    <?php include_once 'footer.php';?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+   <script src="Js/main.js"></script>
+</body>
+</html>
