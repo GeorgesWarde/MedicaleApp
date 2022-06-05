10ad585539370da14da86e7eac5f2dd4ec99769f
anthony4454 <anthony.saliban@outlook.com>
2022-06-05 11:20:06 +0300

    change the directory of the file header and footer

---
+++ require/footer.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>footer</title>
+    <link rel="stylesheet" href="../style/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+   
+    <footer class="footer container-xl">
+        <div class="row">
+            <div class="col-md-3"> 
+                <div class="logo"><a href="#"><img src="./images/bmc_logo_blue.svg"></a></div>
+                <div class="blur">Boston Medical Center (BMC) is a 514-bed academic medical center located in Boston's historic South End, providing medical care for infants, children, teens and adults.</div>
+                <div class="address"><img src="https://www.bmc.org/themes/custom/bmc_base_theme/assets/images/icons/icon-location-pin.svg"><br>
+                    One Boston Medical Center Place<br>Boston, MA 02118<br>
+                    <a href="tel:+617.638.8000">617.638.8000</a>
+                </div>
+                <ul class="inline">
+                    <li class="social-link">
+                        <a href="#"><img src="https://www.bmc.org/themes/custom/bmc_base_theme/assets/images/icons/icon-facebook.svg"></a>
+                    </li>
+                    <li class="social-link">
+                        <a href="#"><img src="https://www.bmc.org/themes/custom/bmc_base_theme/assets/images/icons/icon-instagram.svg"></a>
+                    </li>
+                    <li class="social-link">
+                        <a href="#"><img src="https://www.bmc.org/themes/custom/bmc_base_theme/assets/images/icons/icon-linkedin.svg"></a>
+                    </li>
+                    <li class="social-link">
+                        <a href="#"><img src="https://www.bmc.org/themes/custom/bmc_base_theme/assets/images/icons/icon-twitter.svg"></a>
+                    </li>
+                    <li class="social-link">
+                        <a href="#"><img src="https://www.bmc.org/themes/custom/bmc_base_theme/assets/images/icons/icon-youtube.svg"></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <div class="links">
+                    <a href="http://" class="link">Accreditation Regulatory Compliance</a>
+                    <a href="http://" class="link">Donate</a>
+                    <a href="http://" class="link">Awards</a>
+                    <a href="http://" class="link">Careers</a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="links">
+                    <a href="http://" class="link">Find A Doctor</a>
+                    <a href="http://" class="link">ICU Nurse Ratios</a>
+                    <a href="http://" class="link">Institutional Master Plan</a>
+                    <a href="http://" class="link">Insurance Plans Accepted</a>
+                    <a href="http://" class="link">Medical Records</a>
+                    <a href="http://" class="link">Pricing and Estimates</a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="links">
+                    <a href="http://" class="link">Patient Financial Assistance
+                    </a>
+                    <a href="http://" class="link">Media Relations
+                    </a>
+                    <a href="http://" class="link">Residency
+                    </a>
+                    <a href="http://" class="link">Vendor Access
+                    </a>
+                    <a href="http://" class="link">Volunteer</a>
+                </div>
+            </div>
+        </div>
+        <div class="row" style="margin-top:30px;">
+            <div class="col-md-4 images">
+                 <a href="http://"><img src="./images/net_plan.webp"></a> 
+            </div>
+            <div class="col-md-4 images">
+                  <a href="#"><img src="./images/university.webp"></a>
+            </div>
+            <div class="col-md-4 images">
+                  <a href="#"><img src="./images/health_net.webp"></a>
+            </div>
+            
+        </div>
+        <div class="row">
+            <div class="col-12" style="text-align:center;margin-top:20px">BMC Privacy Policy 
+Code of Conduct 
+Nondiscrimination Statement 
+Patient Privacy/HIPAA 
+Website Terms of Use
+© 2022 Boston Medical Center. All rights reserved.</div>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ require/header.php
@@ -0,0 +1,33 @@
+<header>
+
+    <nav class="navbar navbar-expand-lg navbar-light " aria-label="Fifth navbar example">
+    
+      <div class="container-xl">
+        <a class="navbar-brand" href="index.php">MedicalCenter</a>
+        <button class="navbar-toggler menu-button" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExample05">
+
+          <ul class="navbar-nav f WhiteColor ms-auto">
+ 
+            <li class="nav-item dropdown navbar-items ">
+
+              <a class="nav-link " href="appointment.php" >Make an appointment</a>
+              
+            </li>
+            
+            <li class="nav-item dropdown navbar-items">
+
+              <a class="nav-link" href="login.php">Login</a>
+            </li>
+          </ul>
+
+          
+        </div>
+      </div>
+      
+    </nav>
+    
+  </header>
